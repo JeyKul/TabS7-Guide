91308489210191a506fc48f9b4aaca9ebc5b4ab1
JeyKul <77121325+JeyKul@users.noreply.github.com>
2025-08-08 01:23:32 +0200

    cleanup

--- script.js
+++ script.js
@@ -15,15 +15,15 @@ const steps = [
   { img: "assets/7.png", title: "Step 7", description: "Hold the Volume Up key for a few seconds.", help: null, buttons: [] },
   { img: "assets/8.png", title: "Step 8", description: "To Unlock the bootloader press Volume Up.\n\nWarning: This will FACTORY RESET your device.\n\nPress and hold the Volume buttons again.", help: null, buttons: [] },
   { img: "assets/9.png", title: "Step 9", description: "Press Volume Up to enter the Download Mode", help: null, buttons: [] },
-  { img: "assets/10.png", title: "Step 10", description: "Now We're in Download mode.\nPlease use the 'Downloads' button.", help: null, buttons: [{ name: "Downloads", url: "./pages/odin.html" }]},
-  { img: "assets/11.png", title: "Step 11", description: "Start the Program and make sure your Device is recognized.\nDownload the next Pack.", help: null, buttons: [{ name: "Downloads", url: "./pages/choose.html" }]},
+  { img: "assets/10.png", title: "Step 10", description: "Now We're in Download mode.\nPlease use the 'Downloads' button.", help: null, buttons: [{ name: "Downloads", url: "./pages/odin" }]},
+  { img: "assets/11.png", title: "Step 11", description: "Start the Program and make sure your Device is recognized.\nDownload the next Pack.", help: null, buttons: [{ name: "Downloads", url: "./pages/choose" }]},
   { img: "assets/12.png", title: "Step 12", description: "Add the .tar file into Odin. Position doesn't matter.", help: null, buttons: [] },
   { img: "assets/13.png", title: "Step 13", description: "Press and hold Power + Volume Up. Then press Start in Odin and wait.", help: null, buttons: [] },
   { img: "assets/14.png", title: "Step 14", description: "Welcome to TWRP.", help: null, buttons: [] },
   { img: "assets/15.png", title: "Step 15", description: "Press \"Wipe\".", help: null, buttons: [] },
   { img: "assets/16.png", title: "Step 16", description: "Press \"Format Data\".", help: null, buttons: [] },
   { img: "assets/17.png", title: "Step 17", description: "Type \"yes\" and press blue button at bottom right.", help: null, buttons: [{ name: "Next steps", url: "https://example.com" }]},
-  { img: "assets/14.png", title: "Step 17", description: "And we're done.", help: null, buttons: [{ name: "Next steps (soon)", url: "./pages/next.html" }]},
+  { img: "assets/14.png", title: "Step 17", description: "And we're done.", help: null, buttons: [{ name: "Next steps (soon)", url: "./pages/next" }]},
 ];
 
 let currentStep = 0;
